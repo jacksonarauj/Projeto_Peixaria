fc0efd83855852095d259e10abb571eb1ad7f756
jacksonaraulj <jacksonstm98@gmail.com>
2021-06-02 23:19:12 -0300

    continuando a estilizaçao do menu

--- application/views/cabeçalho.php
+++ application/views/cabeçalho.php
@@ -1,18 +1,15 @@
-<header>    
-   		<div class="col-md-2" id="logo">
+<header>  
+<div class="container" id="menu" >   
+	<nav  class="navbar navbar-expand-lg navbar-dark  ">
+   		<div class=" col-md-2" id="logo">
 			<img src="<?=base_url()?>/assets/Imagens/logo2.jpeg" > 
-		</div> 
-	<div id="menu" > 
-		<ul>  
-	   						
+		</div>
+				
 				<!--<a href="<?= base_url()?>index.php/inicial/registrar"><button id="botao-cadastrar1" type="button" class="btn btn-dark btn-sm">Registrar</button></a>
 				<a href="<?=base_url()?>index.php/usuario/removeUsuario"><button id="botao-cadastrar2" class="btn btn-dark btn-sm">excluirUsuario<i class="fas fa-sign-in-alt"></i></button></a>-->
-				<?php
-					
+				<?php	
 					if(empty($_SESSION['usuario'])){
 				?>
-				<a href="<?= base_url()?>index.php/inicial/registrar"><button id="botao-cadastrar1" type="button" class="btn btn-dark btn-sm">Registrar</button></a>
-				<a href="<?= base_url()?>index.php/usuario/login"><button id="botao-cadastrar2" class="btn btn-dark btn-sm">Login <i class="fas fa-sign-in-alt"></i></button></a>
 				<?php
 					}else{
 						?>
@@ -21,25 +18,36 @@
 						<?php
 					}
 				?>
-				<li> <a  href="<?= base_url()?>index.php/inicial">Home</a></li>
-				<!--<li> <a  href="<?= base_url()?>index.php/usuario/login">Login</a></li>-->
+		<ul class="navbar=nav ml-auto">
+			<li class="nav-item"> 
+				<a lass="nav-link" href="<?= base_url()?>index.php/inicial">Home</a>
+			</li>
+			<li class="nav-item"> 
+				<a lass="nav-link" href="<?= base_url()?>index.php/usuario/login">
+					<button id="botao-login" type="button" class="btn btn-dark btn-sm">  
+						<img id="image-login" src="<?=base_url()?>/assets/Imagens/iconeentrar.png" > 
+					</button>
+				</a>
+			<li class="nav-item"> 
+				<a lass="nav-link"href="<?= base_url()?>index.php/inicial/registrar">
+					<button id="botao-cadastrar" type="button" class="btn btn-dark btn-sm">Cadastre-se	</button>
+				</a>
+			</li>
+			<li class="nav-item"> 
+				<a  href="<?= base_url()?>index.php/usuario/listar">Listar</a>
+			</li>
 				
-				<li> <a  href="<?= base_url()?>index.php/usuario/login">Login</a>
-				</li>
-				<li> <a  href="<?= base_url()?>index.php/usuario/listar">Listar</a></li>
-				
-				
-					<form class="form-buscar">
+		</ul>	
+		<form class="form-inline  ml-auto" 	id="form-buscar">
         	<input class="buscar" type="search" placeholder="" aria-label="Search">
        	    <button class="botao-buscar" type="submit">Buscar</button>
     	</form>
    
                 
-			</ul>
 
-</div>	
+     </div>	
 
-	
+	</nav>
 </div>
 </header>
 <body>
