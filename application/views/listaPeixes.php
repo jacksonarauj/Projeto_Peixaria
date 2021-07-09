a0812ea226f817065ae9c5fefe3cffe2d4d5eda9
jacksonaraulj <jacksonstm98@gmail.com>
2021-07-09 13:33:09 -0300

    criando cards de produtos

--- application/views/listaPeixes.php
+++ application/views/listaPeixes.php
@@ -1,14 +1,31 @@
 
 <div class="container">
-    <<center>
-	<table>
-		<th>ID</th>
-		<th>Nome</th>
-		<th>Preco por KG</th>
-		<th>Habitat</th>
-		<th>&nbsp;</th>
-	<?php foreach ($peixes as $peixe) {		?>
+    
+		
+		<table>
+			<th>ID</th>
+			<th>Nome</th>
+			<th>Preco por KG</th>
+			<th>Habitat</th>
+			<th>&nbsp;</th>
+			<tbody id="resultado">
+				
+			</tbody>
+			</table>
+	
+</div>
+</div>
 
+<script>
+	var resultadoBanco = document.getElementById('resultado')
+	console.log(resultadoBanco)
+
+	resultado.innerHTML = `
+	
+	<?php foreach ($peixes as $peixe) {		?>
+		<div class="card">
+		<div class="card-body">
+		
 		<tr>
 			<td>
 				#<?= $peixe['id_Peixe'] ?>
@@ -23,25 +40,21 @@
 				<?= $peixe['habitat'] ?>
 			</td>
 			<td>
+				
 				<a href="produto/editar/<?= $peixe['id_Peixe'] ?>">
 					<input type="button" name="editar" value="editar">
 				</a>
 			</td>
 		</tr>
-	<?php } ?>
-	</table>
-</center>
+		<br>
+		</tr>
+		</div>
 </div>
-</div>
+<?php } ?>
 
+	`
 
-
-
-
-
-
-
-
+</script>
 
 
 
